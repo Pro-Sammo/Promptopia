6605bb2ae7ad610f089b0fdd2adba7c26b6a136c
Pro-Sammo <ss.sammo2002@gmail.com>
2023-05-24 22:06:23 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      MONGODB_URI: process.env.MONGODB_URI
+    },
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+      },
+      images: {
+        domains: ['lh3.googleusercontent.com'],
+      },
+      webpack(config) {
+        config.experiments = {
+          ...config.experiments,
+          topLevelAwait: true,
+        }
+        return config
+      }
+  };
 
 module.exports = nextConfig
+
+  
